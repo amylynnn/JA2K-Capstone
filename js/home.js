474d0e242cc9c472c4dc261938b7d1489369e05c
Amylynn <143349878+amylynnn@users.noreply.github.com>
2026-05-02 09:01:22 +0400

    Added panel section and images

--- js/home.js
+++ js/home.js
@@ -72,3 +72,74 @@ if (counters.length > 0) {
     counterObserver.observe(counter);
   });
 }
+
+// Wrap every letter
+const textWrapper = document.querySelector('.ml3');
+
+if (textWrapper) {
+  textWrapper.innerHTML = textWrapper.textContent.replace(
+    /\S/g,
+    "<span class='letter'>$&</span>"
+  );
+
+  anime.timeline()
+    .add({
+      targets: '.ml3 .letter',
+      opacity: [0, 1],
+      translateY: [40, 0], // smooth upward motion
+      easing: "easeOutExpo",
+      duration: 1200,
+      delay: (el, i) => 40 * i
+    });
+}
+
+const slider = document.getElementById("servicesSlider");
+
+function updateActiveSlide() {
+  if (!slider) return;
+
+  const slides = slider.querySelectorAll(".service-slide");
+  const sliderCenter = slider.scrollLeft + slider.offsetWidth / 2;
+
+  slides.forEach((slide) => {
+    const slideCenter = slide.offsetLeft + slide.offsetWidth / 2;
+    const distance = Math.abs(sliderCenter - slideCenter);
+
+    if (distance < slide.offsetWidth / 2) {
+      slide.classList.add("active");
+    } else {
+      slide.classList.remove("active");
+    }
+  });
+}
+
+if (slider) {
+  slider.addEventListener("scroll", updateActiveSlide);
+  window.addEventListener("load", updateActiveSlide);
+  window.addEventListener("resize", updateActiveSlide);
+}
+
+const faqItems = document.querySelectorAll(".faq-item");
+
+faqItems.forEach(item => {
+  item.addEventListener("click", () => {
+    item.classList.toggle("active");
+
+    const symbol = item.querySelector("span");
+    symbol.textContent = item.classList.contains("active") ? "-" : "+";
+  });
+});
+
+const panelCards = document.querySelectorAll(".panel-card");
+
+panelCards.forEach(card => {
+  card.addEventListener("click", () => {
+
+    // remove active from all
+    panelCards.forEach(c => c.classList.remove("active"));
+
+    // add active to clicked
+    card.classList.add("active");
+
+  });
+});
